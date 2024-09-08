21691a3a15803c3c63a8132d4412f172a5906794
Jacobtread <jacobtread@gmail.com>
2024-09-08 23:33:43 +1200

    feat: tunnel fallback support

--- src/servers.rs
+++ src/servers.rs
@@ -30,22 +30,40 @@ pub fn start_all_servers(ctx: Arc<ClientContext>) {
     run_server(qos, "qos");
 
     // Spawn tunnel server
-    match ctx.tunnel_port {
-        // When UDP tunnel server port is available use the faster UDP tunnel server
-        Some(tunnel_port) => {
-            let tunnel = udp_tunnel::start_udp_tunnel_server(ctx.clone(), tunnel_port);
-            run_server(tunnel, "tunnel");
-        }
-        // When unavailable fallback to the HTTP upgrade tunnel
-        None => {
-            let tunnel = tunnel::start_tunnel_server(ctx.clone());
-            run_server(tunnel, "tunnel");
-        }
-    };
+    let tunnel = start_tunnel_server(ctx.clone());
+    run_server(tunnel, "tunnel");
 
     // Spawn telemetry server
     let telemetry = telemetry::start_telemetry_server(ctx);
     run_server(telemetry, "telemetry");
+}
+
+/// Runs the tunnel server, if a tunnel port is available a UDP tunnel will be
+/// attempted, if that fails or a tunnel port is unavailable an HTTP tunnel
+/// will be attempted instead
+async fn start_tunnel_server(ctx: Arc<ClientContext>) -> std::io::Result<()> {
+    // Spawn tunnel server
+    match ctx.tunnel_port {
+        // When UDP tunnel server port is available use the faster UDP tunnel server
+        Some(tunnel_port) => {
+            let err = match udp_tunnel::start_udp_tunnel_server(ctx.clone(), tunnel_port).await {
+                // Encountered error with UDP tunnel
+                Err(err) => err,
+                // Server exited normally
+                Ok(_) => return Ok(()),
+            };
+
+            error!(
+                "error using UDP tunnel, falling back to HTTP tunnel: {}",
+                err
+            );
+
+            // Error while connecting UDP tunnel, fallback to HTTP upgrade tunnel
+            tunnel::start_tunnel_server(ctx).await
+        }
+        // When unavailable fallback to the HTTP upgrade tunnel
+        None => tunnel::start_tunnel_server(ctx).await,
+    }
 }
 
 /// Runs the provided server `future` in a background task displaying
